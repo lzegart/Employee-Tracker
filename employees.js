cc09a04f8abb0571a057e72620fbf48cd68ce9e4
LZ <laraezegart@gmail.com>
2021-01-04 14:27:13 -0800

    added connection to my sql server and sql database so inquirer function will start after connection is made

--- employees.js
+++ employees.js
@@ -10,3 +10,20 @@ const connection = mysql.createConnection({
     database: 'employees_DB',
 });
 
+// function to prompt user to enter employee info with inquirer
+const start = () => {
+    inquirer
+        .prompt({
+            name: 'task',
+            type: 'list',
+            message: 'What would you like to do?',
+            choices: ['View all employees', 'Search employee by name', 'Search employee by department', 'Search employee by role', 'Search employee by manager', 'Add employee', 'add department', 'role', 'employee'],
+        })
+}
+
+// connect to the mysql server and sql database
+connection.connect((err) => {
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    start();
+  });
