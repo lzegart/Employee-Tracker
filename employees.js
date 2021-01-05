cb21f3c51f39b25be5730721506cab21fc2ead6f
LZ <laraezegart@gmail.com>
2021-01-04 18:48:38 -0800

    corrected first prompt question and added .then to prompt following Qs

--- employees.js
+++ employees.js
@@ -7,22 +7,39 @@ const connection = mysql.createConnection({
     port: 3306,
     user: 'root',
     password: '',
-    database: 'employees_DB',
+    database: 'employees_db',
 });
 
-// function to prompt user to enter employee info with inquirer
+// function to prompt user to enter/view/edit employee info with inquirer
 const start = () => {
     inquirer
         .prompt({
             name: 'task',
             type: 'list',
             message: 'What would you like to do?',
-            choices: ['View all employees', 'Search employee by name', 'Search employee by department', 'Search employee by role', 'Search employee by manager', 'Add employee', 'add department', 'role', 'employee'],
+            choices: ['View employees', 'Add employee info', 'Update or delete employee info']
+        })
+        .then((answer) => {
+            if (answer.task === 'View employees') {
+                viewEmployee();
+            } else if (answer.task === 'Add employee info') {
+                addEmployee();
+            } else if (answer.task === 'Update or delete employee info') {
+                updateEmployee();
+            }
+        });
+}
+
+['View all employees', 
+            'Search employee by name', 
+            'Search employee by department', 
+            'Search employee by role', 
+            'Search employee by manager', 
+            'Add employee', 
+            'add department', 'role', 'employee'],
             // Add/ view/ update
             // roles/ department/ employee
             // 
-        })
-}
 
 // connect to the mysql server and sql database
 connection.connect((err) => {
