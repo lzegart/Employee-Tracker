09b618064b055ba3174abc375dfdc2c8bc99c75d
LZ <laraezegart@gmail.com>
2021-01-30 15:12:31 -0800

    updated schema

--- schema.sql
+++ schema.sql
@@ -5,15 +5,17 @@ CREATE DATABASE employees_db;
 USE employees_db;
 
 CREATE TABLE department (
-id INT AUTO_INCREMENT KEY,
-name VARCHAR(30) NOT NULL
+id INT NOT NULL AUTO_INCREMENT KEY,
+name VARCHAR(30) NOT NULL,
+PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-id INT AUTO_INCREMENT KEY,
+id INT NOT NULL AUTO_INCREMENT KEY,
 title VARCHAR(30) NOT NULL,
-salary DECIMAL (10,2),
-department_id INT
+salary DECIMAL (10,2) NOT NULL,
+department_id INT,
+PRIMARY KEY (id)
 );
 
 CREATE TABLE employee (
